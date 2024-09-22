d6fdea81c9ee40c77396ef74471b23982c88bf96
mtthw <matbalinton@gmail.com>
2024-09-22 14:54:35 +0800

    Finished some styling and make some changes

--- controllers/MyController.js
+++ controllers/MyController.js
@@ -1,19 +1,7 @@
 const main = {
-    createUser:(req, res) => {
-        res.render('login', {title: 'Authentication', activePage: 'login' })
-    },
-    Home:(req, res) => {
-        res.render('home', {title: 'Homepage', activePage: 'home' })
-    },
-    getFeature:(req, res) => {
-        res.render('feature', { title: 'Services Page', activePage: 'feature' })
-    },
-    getHero:(req, res) => {
-        res.render('hero', {title: 'About Page', activePage: 'hero' })
-    },
-    getPricing:(req, res) => {
-        res.render('pricing', {title: 'Pricing Page', activePage: 'pricing' })
-    }, 
+    Home: (req,res) => {
+        res.render('index')
+    }
 }
 
 module.exports = main;
--- routes/MyRoutes.js
+++ routes/MyRoutes.js
@@ -3,11 +3,11 @@ const MyController = require('../controllers/MyController');
 
 const router = express.Router();
 
-router.get('/login', MyController.createUser);
-router.get('/feature', MyController.getFeature);
-router.get('/hero', MyController.getHero);
+//router.get('/login', MyController.createUser);
+//router.get('/feature', MyController.getFeature);
+//router.get('/hero', MyController.getHero);
 router.get('/', MyController.Home);
-router.get('/pricing', MyController.getPricing);
+//router.get('/pricing', MyController.getPricing);
 
 module.exports = router;
 
